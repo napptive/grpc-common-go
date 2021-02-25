ae8a3bb97dc770945b2868d9f6964f10fb9bdf13
hermione-napptive <cicd@napptive.com>
2021-02-25 08:27:41 +0000

    gpm automatic publish

--- entities.pb.validate.go
+++ entities.pb.validate.go
@@ -168,3 +168,70 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = OpResponseValidationError{}
+
+// Validate checks the field values on ErrorDetails with the rules defined in
+// the proto definition for this message. If any rules are violated, an error
+// is returned.
+func (m *ErrorDetails) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for Detail
+
+	return nil
+}
+
+// ErrorDetailsValidationError is the validation error returned by
+// ErrorDetails.Validate if the designated constraints aren't met.
+type ErrorDetailsValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ErrorDetailsValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ErrorDetailsValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ErrorDetailsValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ErrorDetailsValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ErrorDetailsValidationError) ErrorName() string { return "ErrorDetailsValidationError" }
+
+// Error satisfies the builtin error interface
+func (e ErrorDetailsValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sErrorDetails.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ErrorDetailsValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ErrorDetailsValidationError{}
